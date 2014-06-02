5646f50709cdc147f2aeb37a34b6bd6a3010e031
tomcoop12 <tom.cooper1993@hotmail.co.uk>
2014-06-02 13:51:37 +0100

    changed enemy sprites

--- js/engine.js
+++ js/engine.js
@@ -56,7 +56,7 @@ var GameScreen = function GameScreen(text,text2,callback) {
     canvas.clearRect(0,0,Game.width,Game.height);
     canvas.font = "bold 40px arial";
     var measure = canvas.measureText(text);  
-    canvas.fillStyle = "#FFFFFF";
+    canvas.fillStyle = "#000000";
     canvas.fillText(text,Game.width/2 - measure.width/2,Game.height/2);                //sets the font, text weight and colour of the game
     canvas.font = "bold 20px arial";
     var measure2 = canvas.measureText(text2);
